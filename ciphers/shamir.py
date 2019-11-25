62df4e3201f8c014c73792d49e299859f62f599a
rudeqit <ancherko@gmail.com>
2019-11-26 01:56:11 +0700

    Fix Shamir

--- ciphers/shamir.py
+++ ciphers/shamir.py
@@ -31,7 +31,7 @@ class Shamir:
                 
                 ### Write decode info in file
                 # self.log(x1, x2, x3)
-                fd_out.write(el.to_bytes(num_bytes, byteorder="little"))
+                fd_out.write(x4.to_bytes(num_bytes, byteorder="little"))
 
     def log(self, x1, x2, x3, keys_file = "Shamir_Keys.txt", decode_value = "Shamir_Decode.txt"):
         with open(keys_file, "a") as fd_kf, open(decode_value, "a") as fd_df:
@@ -50,17 +50,17 @@ class Subscriber:
         return pow_mod(message, self.d, self.p)
 
     def get_param(self, p):
-        self.c = get_rand()
+        self.c = get_rand()               
         nod, x, y = steroid_evklid(self.c, p - 1)
 
         while nod != 1 or x <= 0:
             self.c = get_rand()
             nod, x, y = steroid_evklid(self.c, p - 1)
-        self.d = x
+        self.d = x * self.c
 
 
 
 if __name__ == "__main__":
     cipers_shamir = Shamir()
-    cipers_shamir.shamir("pic.jpg", "out_pic.jpg")
-    # cipers_shamir.shamir("1.txt", "2.txt")
+    # cipers_shamir.shamir("pic.jpg", "out_pic.jpg")
+    cipers_shamir.shamir("1.txt", "2.txt")
